34c9278b7593733d55da7021acf81a9142cc45e3
22PHOP2D-Nasanbayar <Howl_bugged@Bayarjavkhlans-mac.local>
2023-01-04 19:47:54 +0800

    sec

--- src/components/Createplaylist.js
+++ src/components/Createplaylist.js
@@ -1,12 +1,37 @@
 import styles from "../styles/createplaylist.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 export const Createplaylist = () => {
-  const [toggle, setToggle] = useState(true);
+  const [title, setTitle] = useState(" ");
+  const [desc, setDesc] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [lists, setLists] = useState([]);
+  const nav = useNavigate();
+  const addList = () => {
+    axios
+      .post("http://localhost:8000/playlists", {
+        title: title,
+        description: desc,
+      })
+      .then((res) => {
+        console.log(res.data);
+        nav("/Createplaylist");
+      });
+    setToggle(false);
+  };
+
+  useEffect(() => {
+    axios.get("http://localhost:8000/playlists").then((res) => {
+      setLists(res.data);
+    });
+  });
+
   return (
     <div>
-        <button className={styles.tog} onClick={() => setToggle(!toggle)}>
-          Create playlist
-        </button>
+      <button className={styles.tog} onClick={() => setToggle(!toggle)}>
+        Create playlist
+      </button>
       <div className={styles.bcontainer}>
         {toggle && (
           <div className={styles.container}>
@@ -15,11 +40,31 @@ export const Createplaylist = () => {
               className={styles.input}
               type="text"
               placeholder="Title"
-            ></input>
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="description"
+              onChange={(e) => setDesc(e.target.value)}
+            />
             <br></br>
-            <button className={styles.button}>create</button>
+            <button onClick={addList} className={styles.button}>
+              create
+            </button>
+
           </div>
         )}
+      </div>
+      <div>
+        {lists.map((item) => {
+          return (
+            <div>
+              <h1>Title : {item.title}</h1>
+              <h1>Description: {item.description}</h1>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
